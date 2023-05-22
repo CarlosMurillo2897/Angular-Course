4292491b9d9bb0e73a2d0140b32845bae5e9157a
cmb2897 <cmb2897@gmail.com>
2023-05-21 23:07:48 -0600

    Adding Server Element & Cockpit Components. Commenting methods while adding logic. Passing Input Parameter to Server Element Component.

--- CMP-DATABINDING-START/src/app/app.module.ts
+++ CMP-DATABINDING-START/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { ServerElementComponent } from './server-element/server-element.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerElementComponent,
+    CockpitComponent
   ],
   imports: [
     BrowserModule,
