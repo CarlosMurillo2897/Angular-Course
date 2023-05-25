1f80a8bc6e28f785f5a2c1c81a3bf51d9e89e2e2
CarlosMurillo2897 <CarlosMurillo2897>
2023-05-25 09:49:44 -0600

    LifeCycle methods implemented.

--- CMP-DATABINDING-START/src/app/server-element/server-element.component.ts
+++ CMP-DATABINDING-START/src/app/server-element/server-element.component.ts
@@ -1,12 +1,66 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated // None, Native.
 })
-export class ServerElementComponent {
-  @Input('srvElement') element: { type: string, name: string, content: string };
+export class ServerElementComponent implements OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
+  @Input('srvElement') element: { type: string; name: string; content: string };
 
-  
+  constructor() {
+    this.log('Constructor');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.log('ngOnChanges');
+    console.log(changes);
+    
+  }
+  ngDoCheck(): void {
+    this.log('ngDoCheck');
+  }
+  ngAfterContentInit(): void {
+    this.log('ngAfterContentInit');
+  }
+  ngAfterContentChecked(): void {
+    this.log('ngAfterContentChecked');
+  }
+  ngAfterViewInit(): void {
+    this.log('ngAfterViewInit');
+  }
+  ngAfterViewChecked(): void {
+    this.log('ngAfterViewChecked');
+  }
+  ngOnDestroy(): void {
+    this.log('ngOnDestroy');
+  }
+  ngOnInit(): void {
+    this.log('ngOnInit');
+  }
+  log(methodName): void {
+    console.log(`${methodName} Init!`);
+  }
 }
