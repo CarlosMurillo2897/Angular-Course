6f062d63464c5e85097bb9b278683e07812b32f1
cmb2897 <cmb2897@gmail.com>
2023-05-27 23:43:00 -0600

    Adding Shopping List to Module global Providers. Removing unnecessary code replaced by Service.

--- TheBasics/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ TheBasics/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -9,11 +10,12 @@ import { Ingredient } from 'src/app/shared/ingredient.model';
 export class ShoppingEditComponent {
   @ViewChild('nameInput') nameInputRef: ElementRef;
   @ViewChild('amountInput') amountInputRef: ElementRef;
-  @Output() ingredientAdded = new EventEmitter<{ name: string, amount: number }>();
+
+  constructor(private slService: ShoppingListService) {}
 
   onAddItem() {
     const ingName = this.nameInputRef.nativeElement.value;
     const ingAmount = this.amountInputRef.nativeElement.value;
-    this.ingredientAdded.emit( new Ingredient(ingName, ingAmount) );
+    this.slService.addIngredient( new Ingredient(ingName, ingAmount) );
   }
 }
