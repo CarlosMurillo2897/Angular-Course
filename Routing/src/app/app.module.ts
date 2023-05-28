4d0c60386f4271cc0dad6750aa255a514f233116
cmb2897 <cmb2897@gmail.com>
2023-05-28 17:19:17 -0600

    Adding SubComponents.

--- Routing/src/app/app.module.ts
+++ Routing/src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ServersComponent } from './servers/servers.component';
 import { UsersComponent } from './users/users.component';
+import { UserComponent } from './users/user/user.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ServersComponent,
-    UsersComponent
+    UsersComponent,
+    UserComponent,
+    EditServerComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule
