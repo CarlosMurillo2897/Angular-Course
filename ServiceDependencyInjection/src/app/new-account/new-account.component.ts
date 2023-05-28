c76e858cf6f0715385a4303e21da6e1cd6f3a6f2
cmb2897 <cmb2897@gmail.com>
2023-05-27 20:12:44 -0600

    Adding Accounts Service. Replacing Event Emitters with Account Service.

--- ServiceDependencyInjection/src/app/new-account/new-account.component.ts
+++ ServiceDependencyInjection/src/app/new-account/new-account.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { LoggingService } from '../services/logging.service';
+import { AccountsService } from '../services/accounts.service';
 
 @Component({
   selector: 'app-new-account',
@@ -10,13 +11,10 @@ import { LoggingService } from '../services/logging.service';
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{ name: string, status: string }>();
 
-  constructor(private loggingService: LoggingService) {}
+  constructor(private loggingService: LoggingService, private accountsService: AccountsService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
+    this.accountsService.addAccount(accountName, accountStatus);
     this.loggingService.logStatusChange(accountStatus);
   }
 }
