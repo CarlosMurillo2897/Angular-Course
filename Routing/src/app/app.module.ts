b4c88bbfa409ba90175ee3edb09dd326a16670af
cmb2897 <cmb2897@gmail.com>
2023-05-28 17:17:59 -0600

    Adding Components.

--- Routing/src/app/app.module.ts
+++ Routing/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServersComponent } from './servers/servers.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServersComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule
