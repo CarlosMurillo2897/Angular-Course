5bb8294e0d24f64c8dbedae8e68c75f6ac294c4c
cmb2897 <cmb2897@gmail.com>
2023-05-28 12:23:15 -0600

    Adding Ingredients to Recipes. Adding real data to Recipes. Displaying Recipe Ingredients.

--- TheBasics/src/app/recipes/recipe.service.ts
+++ TheBasics/src/app/recipes/recipe.service.ts
@@ -1,14 +1,31 @@
-import { EventEmitter } from "@angular/core";
-import { Recipe } from "./recipe.model";
+import { EventEmitter } from '@angular/core';
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
-    private recipes: Recipe[] = [
-        new Recipe('A test Receipe', 'Simple Test.', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505'),
-        new Recipe('Second Receipe', 'Dumb Test.', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505')
-    ];
+  recipeSelected = new EventEmitter<Recipe>();
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://www.196flavors.com/wp-content/uploads/2021/11/FP.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]
+    ),
+    new Recipe(
+      'Big Fat Burger',
+      'What else do you need to say?',
+      'https://assets.bonappetit.com/photos/57aceacc1b3340441497532d/master/w_2700,h_1621,c_limit/double-rl-ranch-burger.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1),
+      ]
+    ),
+  ];
 
-    getRecipes() {
-        return this.recipes.slice();
-    }
+  getRecipes() {
+    return this.recipes.slice();
+  }
 }
