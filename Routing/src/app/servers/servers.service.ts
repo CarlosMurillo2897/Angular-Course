5e4fd0629056ce78fc3e62f0953a891a125cc2f4
cmb2897 <cmb2897@gmail.com>
2023-05-28 20:49:22 -0600

    Adding getServer & updateServer methods into Service. Displaying data according Server.

--- Routing/src/app/servers/servers.service.ts
+++ Routing/src/app/servers/servers.service.ts
@@ -1,11 +1,25 @@
 export class ServersService {
     private servers = [
-        { name: 'Productionserver' },
-        { name: 'Testserver' },
-        { name: 'Devserver' },
+        { id: 1, name: 'Productionserver', status: 'Offline' },
+        { id: 2, name: 'Testserver', status: 'Unknown' },
+        { id: 3, name: 'Devserver', status: 'Online' },
     ];
 
     getServers() {
         return this.servers.slice();
+    }
+    getServer(id: number) {
+        return this.servers.find(
+            (s) => s.id === id
+        );
+    }
+    updateServer(id: number, serverInfo: { name: string, status: string }) {
+        const server = this.servers.find(
+            (s) => s.id === id
+        );
+        if(server) {
+            server.name = serverInfo.name;
+            server.status = serverInfo.status;
+        }
     }
 }
