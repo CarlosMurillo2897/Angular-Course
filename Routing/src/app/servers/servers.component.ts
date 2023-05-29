92dd4ddd7de34f345e2087fc4ca793c1ab35cffb
cmb2897 <cmb2897@gmail.com>
2023-05-28 19:17:27 -0600

    Adding Relative Paths to Programmed routes.

--- Routing/src/app/servers/servers.component.ts
+++ Routing/src/app/servers/servers.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServersService } from './servers.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent {
-  servers = [
-    { name: 'Productionserver' },
-    { name: 'Testserver' },
-    { name: 'Devserver' },
-  ];
+export class ServersComponent implements OnInit {
+  servers = [];
+  
+  constructor(
+    private serversService: ServersService, 
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.servers = this.serversService.getServers();
+  }
+
+  onReload() {
+    // this.router.navigate(['servers'], { relativeTo: this.route });
+  }
+  
 }
