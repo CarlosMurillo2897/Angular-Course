ebe65f67e1bc0f550b94ee6013434ebded45b9c5
cmb2897 <cmb2897@gmail.com>
2023-06-06 19:54:05 -0600

    Adding User Service, using Subject as Emitter. Subscribing to User Service, in order to get Subject.

--- Observables/src/app/app.component.ts
+++ Observables/src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { UserService } from './user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: [`.container {
-      margin-top: 30px;
-    }
-  `]
+  styles: [
+    `
+      .container {
+        margin-top: 30px;
+      }
+    `,
+  ],
 })
-export class AppComponent {
-  title = 'Observables';
+export class AppComponent implements OnInit, OnDestroy {
+  userActivated = false;
+  private activatedSub: Subscription;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.activatedSub = this.userService.activatedEmitter.subscribe(
+      didActivate => this.userActivated = didActivate
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.activatedSub.unsubscribe();
+  }
 }
--- Observables/src/app/user/user.component.ts
+++ Observables/src/app/user/user.component.ts
@@ -1,20 +1,31 @@
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user',
-  template: '<p>user with <b>ID {{ id }}</b> was loaded</p>',
+  template: `
+    <p>
+      user with <b>ID {{ id }}</b> was loaded
+    </p>
+    <button class="btn btn-primary" (click)="onActivate()">Activate</button>
+  `,
   // styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
   id: number = 0;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService
+  ) {}
 
   ngOnInit() {
-    this.route.params.subscribe(
-      (params: Params) => this.id = +params.id
-    );
+    this.route.params.subscribe((params: Params) => (this.id = +params.id));
+  }
+
+  onActivate() {
+    this.userService.activatedEmitter.next(true);
   }
 }
